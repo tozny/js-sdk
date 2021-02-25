cdeb10d0e8812c302b11515ebae36eb84cefad0b
Katie Rosas <katie@tozny.com>
2021-02-24 18:01:34 -0800

    js support for private realm info endpoint

--- __tests__/identity.test.js
+++ __tests__/identity.test.js
@@ -3,7 +3,11 @@ const { apiUrl, idRealmName, idAppName, clientRegistrationToken } = global
 const Tozny = require('../node')
 const ops = require('./utils/operations')
 
+<<<<<<< HEAD
 jest.setTimeout(10000000)
+=======
+jest.setTimeout(100000)
+>>>>>>> 57aebda... js support for private realm info endpoint
 
 let realmConfig
 let realm
@@ -103,4 +107,37 @@ describe('Tozny identity client', () => {
     }
     expect(response).toMatchObject(expectedData)
   })
+  it('it can get private realm info', async () => {
+    const info = await ops.privateRealmInfo(realmConfig, identity)
+    console.log(info)
+  })
 })
+
+// describe('Tozny', () => {
+//   it('skip identity tests (validate test environment)', async () => {
+//     const result = await global.runInEnvironment(function (boolJSON) {
+//       var boolVal = JSON.parse(boolJSON)
+//       return boolVal
+//     }, JSON.stringify(true))
+//     return expect(JSON.parse(result)).toBe(true)
+//   })
+// it('can register an identity client', async () => {
+//   const user = await ops.registerIdentity(realmConfig, realm)
+//   // Basic exist check to ensure we got back a full identity user.
+//   expect(user.config).toMatchObject(realmConfig)
+//   expect(user.config.username).toEqual(
+//     expect.stringMatching(/^integration-user-/)
+//   )
+//   expect(user.storage).toBeInstanceOf(Tozny.storage.Client)
+// })
+// it('can log in to a realm', async () => {
+//   const user = await ops.login(realmConfig, realm, username, password)
+//   expect(user.serialize()).toMatchObject(identity.serialize())
+// })
+// it('can fetch a token', async () => {
+//   const info = await ops.userMethod(realmConfig, identity, 'tokenInfo')
+//   expect(info.access_token).toBeTruthy()
+//   const token = await ops.userMethod(realmConfig, identity, 'token')
+//   expect(token).toBeTruthy()
+//   // })
+// })
