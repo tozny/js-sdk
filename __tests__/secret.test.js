cda0c1fa66262de7617fc548ce116058e7f6fa58
Lily Tsou <lily@tozny.com>
2022-11-28 11:23:44 -0800

    [PLAT-1113] updates timeout for longrunning secrets test

--- __tests__/secret.test.js
+++ __tests__/secret.test.js
@@ -698,6 +698,7 @@ describe('Tozny identity client', () => {
     )
   })
   it('can delete all secrets created by an identity', async () => {
+    jest.setTimeout(200000);
     let listedSecrets = await ops.getSecrets(realmConfig, identity, 100)
     for (let index = 0; index < listedSecrets.list.length; index++) {
       let deleted = await ops.deleteSecretVersion(
